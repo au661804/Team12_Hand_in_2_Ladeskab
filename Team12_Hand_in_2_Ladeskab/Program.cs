e304a533f637c30be238191f5fa9d195c015ccbd
Cecilie Lynggaard Jeppesen <au661804@uni.au.dk>
2022-03-03 16:51:33 +0100

    Add project files.

---
+++ Team12_Hand_in_2_Ladeskab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Team12_Hand_in_2_Ladeskab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
