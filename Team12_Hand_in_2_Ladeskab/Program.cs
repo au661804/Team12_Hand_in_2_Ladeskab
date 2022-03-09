97a54d70940ba3e285e244d96d110394d671af24
Cecilie Lynggaard Jeppesen <au661804@uni.au.dk>
2022-03-09 15:44:34 +0100

    program

--- Team12_Hand_in_2_Ladeskab/Program.cs
+++ Team12_Hand_in_2_Ladeskab/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace Team12_Hand_in_2_Ladeskab
-{
-    class Program
+﻿    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			
+
+
         }
     }
-}
+
