841b70f18a00876af692ee0c94a716bbe8934d4b
Cecilie Lynggaard Jeppesen <au661804@uni.au.dk>
2022-03-10 19:38:13 +0100

    Kode til Display tilføjet

--- Team12_Hand_in_2_Ladeskab/Display.cs
+++ Team12_Hand_in_2_Ladeskab/Display.cs
@@ -1,10 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Channels;
 
 namespace Team12_Hand_in_2_Ladeskab
 {
-    internal class Display
+    class Display
     {
+        private bool isConnected = false;
+        private bool readRFID = false;
+
+        public void ViewCharge(int battery)
+        {
+            if (isConnected == true)
+            {
+                Console.WriteLine(battery + "%");
+            }
+            else
+            {
+                Console.WriteLine("Error connecting phone - try again");
+            }
+
+        }
+
+        public void ViewIsAvailable()
+        {
+            if (isConnected == true)
+            {
+                Console.WriteLine("Box is occupied");
+            }
+            else
+            {
+                Console.WriteLine("Box is available");
+            }
+            
+        }
+
+        public void ViewReadID()
+        {
+            Console.WriteLine("Scan RFID");
+            if (readRFID == false)
+            {
+                Console.WriteLine("RFID failed");
+            }
+            else
+            {
+                Console.WriteLine("Door open");
+            }
+            
+        }
+
+
+        public void View()
+        {}
     }
 }
