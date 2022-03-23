6c6fff6fbc0b5d0d4ace125daf549d2e607ddb7f
Torbjørg <torbjorg-hentze@hotmail.com>
2022-03-23 15:36:27 +0100

    .

--- Test_Hand_in_2_Team12/UnitTestChargeControl.cs
+++ Test_Hand_in_2_Team12/UnitTestChargeControl.cs
@@ -59,19 +59,49 @@ namespace Test_Hand_in_2_Team12
         [TestCase(1)]
         [TestCase(3)]
         [TestCase(5)]
-        public void FullyChargedPhone_DisplayShowDoneCharging(int telefonOpladt)
+        public void FullyChargedPhone_DisplayShowDoneCharging(int fullyCharged)
         {
-            _usbCharger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs() { Current = telefonOpladt });
+            _usbCharger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs() { Current = fullyCharged });
             _display.Received(1).ViewDoneCharging();
         }
 
         [TestCase(-5)]
         [TestCase(100)]
         [TestCase(6)]
-        public void FullyChargedPhone_DisplayShowDoneCharging_withOutsideValues(int telefonOpladt)
+        public void FullyChargedPhone_DisplayShowDoneCharging_withOutsideValues(int fullyCharged)
         {
-            _usbCharger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs() { Current = telefonOpladt });
+            _usbCharger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs() { Current = fullyCharged });
             _display.Received(0).ViewDoneCharging();
+        }
+        [TestCase(6)]
+        [TestCase(150)]
+        [TestCase(500)]    
+        public void PhoneCurrentlyCharging_withCorrectValues(int PhoneCharging)
+        {
+            _usbCharger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs() { Current = PhoneCharging });
+            _display.Received(1).ViewCharging();
+        }
+        [TestCase(5)]
+        [TestCase(501)]
+        [TestCase(-10)]
+        public void PhoneCurrentlyCharging_withInCorrectValues(int PhoneCharging)
+        {
+            _usbCharger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs() { Current = PhoneCharging });
+            _display.Received(0).ViewCharging();
+        }
+        [TestCase(501)]
+        [TestCase(1000)]
+        public void ShortCircuit_withCorrectValues(int ShortCircuit)
+        {
+            _usbCharger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs() { Current = ShortCircuit });
+            _display.Received(1).ViewFailedConnection();
+        }
+        [TestCase(500)]
+        [TestCase(-10)]
+        public void ShortCircuit_withInCorrectValues(int ShortCircuit)
+        {
+            _usbCharger.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs() { Current = ShortCircuit });
+            _display.Received(0).ViewFailedConnection();
         }
 
 
